0143b52ecddf04302410401696205ed0a2d174a0
Sergey P <seregapanin23703@gmail.com>
2022-08-22 11:40:29 +0300

    Выполнил дз

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+//First ex
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
 
+        String fullName = firstName + middleName + lastName;
+//Second ex
+        String fullNameUpCase = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника -" + fullNameUpCase);
 
+        //Exercise 3
+
+        String fullName1 = "Иванов Семен Семенович";
+        fullName1 = fullName1.replace('ё', 'е');
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета " + fullName1);
 
 
     }
